b60fbfdc9cfa376f1188baba834607300b1ab83f
DuarteCamilo <cduartrivera@gmail.com>
2024-10-11 05:58:47 -0500

    add auth

--- FastApi/app/routes/user_routes.py
+++ FastApi/app/routes/user_routes.py
@@ -3,9 +3,11 @@ This module handles the routes for managing users,
 allowing the creation, retrieval, updating, and deletion of user accounts.
 """
 
-from fastapi import APIRouter, Body, HTTPException
+import os
+from fastapi import APIRouter, Body, HTTPException, Depends
 from schemas.user_schema import User
 from config.database import UserModel
+from helpers.apy_key_auth import get_api_key
 
 user_route = APIRouter()
 
@@ -20,13 +22,13 @@ async def create_user(user: User = Body(...)):
     )
     return {"message": "User created successfully"}
 
-@user_route.get("/")
+@user_route.get("/",dependencies=[Depends(get_api_key)])
 async def read_all_users():
     """Retrieve a list of all users."""
     users = UserModel.select().dicts()
     return list(users)
 
-@user_route.get("/{user_id}")
+@user_route.get("/{user_id}" , dependencies=[Depends(get_api_key)])
 async def read_user(user_id: int):
     """Retrieve a specific user by their ID."""
     try:
@@ -35,7 +37,7 @@ async def read_user(user_id: int):
     except Exception as exc:
         raise HTTPException(status_code=404, detail="User not found") from exc
 
-@user_route.put("/{user_id}")
+@user_route.put("/{user_id}" , dependencies=[Depends(get_api_key)])
 async def update_user(user_id: int, user: User = Body(...)):
     """Update an existing user's information."""
     try:
@@ -49,10 +51,17 @@ async def update_user(user_id: int, user: User = Body(...)):
     except Exception as exc:
         raise HTTPException(status_code=404, detail="User not found") from exc
 
-@user_route.delete("/{user_id}")
+@user_route.delete("/{user_id}", dependencies=[Depends(get_api_key)])
 async def delete_user(user_id: int):
     """Delete a user by their ID."""
     rows_deleted = UserModel.delete().where(UserModel.id == user_id).execute()
     if rows_deleted:
         return {"message": "User deleted successfully"}
     raise HTTPException(status_code=404, detail="User not found")
+
+@user_route.post("/login", dependencies=None)
+async def login(email:str, password_hash:str):
+    user = UserModel.get(UserModel.email == email)
+    if user:
+        if user.password_hash == password_hash:
+            return os.getenv("API_KEY")
